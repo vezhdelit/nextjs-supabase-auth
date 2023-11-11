72327844b7019904624408f4c79ad07df2119a99
Vezhdel Vasyl <vezhdelit@gmail.com>
2023-11-11 23:24:05 +0200

    init: basic auth logic implemented

---
+++ components/ui/form/auth-form.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/types/supabase";
+
+export default function AuthForm() {
+  const supabase = createClientComponentClient<Database>();
+
+  return (
+    <Auth
+      supabaseClient={supabase}
+      socialLayout="horizontal"
+      appearance={{
+        theme: ThemeSupa,
+        variables: {
+          default: {
+            colors: {
+              brand: "#0EA5E9",
+              brandAccent: "#0EA5E9",
+            },
+          },
+        },
+      }}
+      providers={["google", "github"]}
+      redirectTo="http://localhost:3000/auth/callback"
+    />
+  );
+}
